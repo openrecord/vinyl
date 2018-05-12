583bd0056eb476517dc9879d85c0c961a86c17c5
osebo <osebo@yourcamel.com>
2018-05-12 13:13:04 -0400

    Add SCSS to React App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+require('./stylesheets/base.scss');
+
 const Index = () => {
   return <div>Hello React!</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
+``
--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,13 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS
+        ]
+      },
     ]
   },
   plugins: [htmlWebpackPlugin]
