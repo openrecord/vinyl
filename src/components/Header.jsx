0eaecdb220e8c3e127679dbea9295b5544c058f9
Ryan Luu <ryan@vumble.com>
2018-05-14 19:33:25 -0400

    Converted to JSX, Added Empty Redux Store, Created Component Folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ export default class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render(){
+        return(
+        <div className="header">
+            <h1>Open Record</h1>
+        </div>
+        )
+    }
+ }
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,7 @@
+const INITIAL_STATE = {
+
+  }
+  
+export default function auth(state = INITIAL_STATE, action) {
+    
+  }
