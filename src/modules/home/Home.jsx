503a22ddabf898701fee39307dacf30d777caa1e
Ryan Luu <ryan@vumble.com>
2018-07-02 20:53:15 -0400

    Click to Enter Landing

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 import UniplayerContainer from '../uniplayer/UniplayerContainer';
-import LandingContainer from '../landing/landingContainer'
+import Landing from '../landing/Landing.jsx'
 
-export default function Home({}) {
-  return (
-    <main>
-      <UniplayerContainer/>
-      <LandingContainer/>
-    </main>
-  );
+export default class Home extends React.Component {
+
+  constructor(props) {
+   super(props);
+
+   this.state = {
+     showLanding: true
+    }
+  }
+
+  closeLanding = () =>{
+    this.setState({showLanding: false});
+  }
+
+  renderLanding = () =>{
+    if(this.state.showLanding){
+      return(
+        <Landing closeLanding={this.closeLanding}/>
+      );
+    }
+  }
+
+  render(){
+    return (
+      <main>
+        <UniplayerContainer/>
+        {this.renderLanding()}
+      </main>
+    );
+  }
 }
--- src/modules/landing/Landing.jsx
+++ src/modules/landing/Landing.jsx
@@ -1,65 +1,76 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default function Landing({}) {
-  return (
-    <LandingBox/>
-  );
+export default class Landing extends React.Component {
+
+  constructor(props) {
+   super(props);
+
+   this.state = {
+    }
+  }
+
+  closeLanding = () =>{
+    this.props.closeLanding();
+  }
+
+  render(){
+    return (
+      <LandingInner>
+        <h1>Listen</h1>
+        <Circle id="c1" onClick={this.closeLanding} />
+        <Circle id="c2" />
+        <Circle id="c3" />
+        <Circle id="c4" />
+        <Circle id="c5" />
+        <Circle id="c6" />
+      </LandingInner>
+    );
+  }
 }
 
-function LandingBox() {
-  const LandingInner = styled.div`
-    background: #9C4D9D;
-    left: 0;
-    right: 0;
-    top: 0;
-    bottom: 0;
-    position: fixed;
-    z-index: 3;
+//Styling
+const LandingInner = styled.div`
+  background: #9C4D9D;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  position: fixed;
+  z-index: 3;
 
-    h1{
-      color: #090C15;
-      line-height: normal;
-      font-size: 48px;
-      font-weight: 700;
-      text-align: center;
-      left: 50%;
-      top: 50%;
-      position: absolute;
-      transform: translate(-50%, -50%);
-      z-index: 8;
-    }
-  `;
-
-  const Circle = styled.div`
-    border-radius: 50%;
+  h1{
+    color: #090C15;
+    cursor: pointer;
+    font-size: 48px;
+    font-weight: 700;
     left: 50%;
-    top: 50%;
+    line-height: normal;
     position: absolute;
+    text-align: center;
+    top: 50%;
     transform: translate(-50%, -50%);
+    z-index: 8;
+  }
+`;
 
-    &#c1{
-      width: 24%;
-      padding-top: 24%;
-      background: #FFFFFF;
-      z-index: 7;
-    }
-    &#c2{ width: 40%; padding-top: 40%; background: #FCD731; z-index: 6; }
-    &#c3{ width: 56%; padding-top: 56%; background: #FBB12B; z-index: 5; }
-    &#c4{ width: 72%; padding-top: 72%; background: #FB8235; z-index: 4; }
-    &#c5{ width: 88%; padding-top: 88%; background: #F8785D; z-index: 3; }
-    &#c6{ width: 100%; padding-top: 100%; background: #BC5BA1; z-index: 2; }
-  `;
+const Circle = styled.div`
+  border-radius: 50%;
+  left: 50%;
+  top: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
 
-  return (
-    <LandingInner>
-      <h1>Listen</h1>
-      <Circle id="c1" />
-      <Circle id="c2" />
-      <Circle id="c3" />
-      <Circle id="c4" />
-      <Circle id="c5" />
-      <Circle id="c6" />
-    </LandingInner>
-  );
-}
+  &#c1{
+    background: #FFFFFF;
+    cursor: pointer;
+    padding-top: 24%;
+    width: 24%;
+    z-index: 7;
+  }
+  &#c2{ width: 40%; padding-top: 40%; background: #FCD731; z-index: 6; }
+  &#c3{ width: 56%; padding-top: 56%; background: #FBB12B; z-index: 5; }
+  &#c4{ width: 72%; padding-top: 72%; background: #FB8235; z-index: 4; }
+  &#c5{ width: 88%; padding-top: 88%; background: #F8785D; z-index: 3; }
+  &#c6{ width: 100%; padding-top: 100%; background: #BC5BA1; z-index: 2; }
+`;
