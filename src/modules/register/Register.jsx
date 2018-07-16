7adcf667b8da6f42bd8d05e7222a3780e7e4b8df
Ryan Luu <ryan@vumble.com>
2018-07-15 22:05:35 -0700

    Added Registration Arrow & Black Background

--- src/modules/register/Register.jsx
+++ src/modules/register/Register.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch, Link} from 'react-router-dom';
 import styled from 'styled-components';
 import _ from 'lodash';
+import arrow from './images/registration-arrow.svg';
 
 import RegisterDTO from './RegisterDTO';
 
@@ -18,7 +19,7 @@ export default function Register(props) {
 
 
 const RegisterFormContainer = styled.main`
-	background: #08368D;
+	background: black;
 	position: absolute;
 	text-align: center;
 	bottom: 0;
@@ -85,10 +86,19 @@ const CheckBoxWrapper = styled.div`
 	}
 `;
 
-	const Form = styled.form`
+const Form = styled.form`
+	background: 0;
+	bottom: 0;
+	left: 0;
+	position: fixed;
+	right: 0;
+	text-align: left;
+	top: 0;
+
+	.form-inner{
+		display: block;
 		left: 50%;
 		position: absolute;
-		text-align: left;
 		top: 50%;
 		transform: translate(-50%, -50%);
 
@@ -99,9 +109,15 @@ const CheckBoxWrapper = styled.div`
 				display: block;
 			}
 
+			&.focus{
+				label{
+					opacity: 0.5;
+				}
+			}
+
 			label {
 				color: white;
-				font-size: 42px;
+				font-size: 2.625em;
 			}
 
 			input {
@@ -111,39 +127,31 @@ const CheckBoxWrapper = styled.div`
 					background: none;
 					border: none;
 					border-bottom: 4px solid white;
+					color: white;
 					display: block;
-					font-size: 42px;
-					line-height: 42px;
+					font-size: 2.625em;
+					line-height: 94px;
 					outline: none;
-					padding: 32px 0;
 					width: 545px;
 				}
 			}
-
-			input[type='checkbox'] {
-				margin: 0;
-			}
 		}
 
-		.step, button{
-			background: none;
-			border: 2px solid white;
-			color: white;
+		.step{
+			background: url(${arrow});
+			background-size: 60px 26px;
+			background-position: center;
+			background-repeat: no-repeat;
 			cursor: pointer;
 			display: none;
-			font-size: 20px;
-			margin-top: 24px;
-			outline: none;
-			padding: 8px 16px;
+			height: 26px;
+			margin-top: 32px;
+			opacity: 0.5;
+			padding: 8px 0;
+			width: 60px;
 
 			&:hover{
-				background: white;
-				color: #08368D;
-			}
-
-			&.show-submit{
-				display: inline-block;
-				float: right;
+				opacity: 1;
 			}
 			&.show-next{
 				display: inline-block;
@@ -151,15 +159,66 @@ const CheckBoxWrapper = styled.div`
 			}
 			&.show-back{
 				display: inline-block;
+				float: left;
+				transform: scale(-1);
 			}
 		}
-	`;
+
+		button{
+			background: none;
+			border: 0;
+			color: white;
+			cursor: pointer;
+			display: none;
+			float: right;
+			font-size: 2.625em;
+			font-weight: 700;
+			margin-top: 24px;
+			opacity: 0.5;
+			outline: none;
+			padding: 0;
+
+			&:hover{
+				opacity: 1;
+			}
+
+			&.show-submit{
+				display: inline-block;
+			}
+
+		}
+	}
+
+	.page-numbers{
+		bottom: 20px;
+		position: fixed;
+		opacity: 0.5;
+		right: 20px;
+
+		span{
+			display: inline-block;
+			color: white;
+			font-size: 1.875em;
+
+			&.dash{
+				background: white;
+				height: 2px;
+				margin: 0 3px;
+				position: relative;
+				top: -10px;
+				width: 20px;
+			}
+		}
+	}
+
+
+`;
 
 function Input(props) {
 	return (
 		<div className={'input-container' + props.class}>
 			<label htmlFor={props.name}>{props.name}</label>
-			<input type={props.type} name={props.name} id={props.name} autoComplete={props.autoComplete} />
+			<input type={props.type} name={props.name} id={props.name} autoComplete={props.autoComplete} onFocus={props.onFocus} onBlur={props.onBlur} />
 		</div>
 	);
 }
@@ -174,6 +233,9 @@ class RegisterForm extends React.Component {
 			nameActive: true,
 			emailActive: false,
 			passwordActive: false,
+			nameFocus: false,
+			emailFocus: false,
+			passwordFocus: false,
 			name: '',
 			email: '',
 			password: '',
@@ -222,57 +284,105 @@ class RegisterForm extends React.Component {
 			name: e.target.value
 		});
 	};
-
 	emailChanged = e => {
 		this.setState({
 			name: e.target.value
 		});
 	};
-
 	passwordChanged = e => {
 		this.setState({
 			password: e.target.value
 		});
 	};
 
+	focusName = () => {
+		this.setState({
+			nameFocus: true
+		});
+	}
+	focusEmail = () => {
+		this.setState({
+			emailFocus: true
+		});
+	}
+	focusPassword = () => {
+		this.setState({
+			passwordFocus: true
+		});
+	}
+
+	blurName = () => {
+		this.setState({
+			nameFocus: false
+		});
+	}
+	blurEmail = () => {
+		this.setState({
+			emailFocus: false
+		});
+	}
+	blurPassword = () => {
+		this.setState({
+			passwordFocus: false
+		});
+	}
+
 	render() {
 
-		var status = {};
-		if (this.state.nameActive){status.name = ' active';}	else {status.name = '';}
-		if (this.state.emailActive){status.email = ' active';} else {status.email = '';}
-		if (this.state.passwordActive){status.password = ' active'; status.submit = ' show-submit'} else {status.password = ''; status.submit = ''}
+		var status = {},
+				focus = {},
+				page = {};
+		if (this.state.nameActive){status.name = ' active'; page.number='1';}	else {status.name = '';}
+		if (this.state.emailActive){status.email = ' active'; page.number='2';} else {status.email = '';}
+		if (this.state.passwordActive){status.password = ' active'; page.number='3'; status.submit = ' show-submit'} else {status.password = ''; status.submit = ''}
 		if (this.state.nameActive || this.state.emailActive){status.next = ' show-next'} else{status.next = ''}
 		if (this.state.emailActive || this.state.passwordActive){status.back = ' show-back'} else{status.back = ''}
+		if (this.state.nameFocus){focus.name = ' focus';} else{focus.name = ''};
+		if (this.state.emailFocus){focus.email = ' focus';} else{focus.email = ''};
+		if (this.state.passwordFocus){focus.password = ' focus';} else{focus.password = ''};
+
 		return (
 			<Form onSubmit={this.submit}>
-				<Input component={this.renderInput} class={status.name}type={'text'} name={"What's your name?"} autoComplete={'given-name'} />
-				<Input component={this.renderInput} class={status.email} type={'email'} name={"What's your email?"} autoComplete={'email'} />
+				<div className="form-inner">
+					<Input class={status.name + focus.name} onFocus={this.focusName} onBlur={this.blurName} type={'text'} name={"What's your name?"} autoComplete={'given-name'} />
+					<Input class={status.email + focus.email} onFocus={this.focusEmail} onBlur={this.blurEmail} type={'email'} name={"What's your email?"} autoComplete={'email'} />
 
-				<div className={'input-container' + status.password}>
-					<label htmlFor={'password'}>Create your password</label>
-					<input
-						type={this.state.showPassword ? 'text' : 'password'}
-						name={'password'}
-						id={'password'}
-						autoComplete={'current-password'}
-						value={this.state.password}
-						onChange={this.passwordChanged}
-					/>
-
-				<CheckBoxWrapper className="password-toggle">
+					<div className={'input-container' + status.password + focus.password}>
+						<label htmlFor={'password'}>Create your password</label>
 						<input
-							type={'checkbox'}
-							name={'showPassword'}
-							id={'showPassword'}
-							checked={this.state.showPassword}
-							onChange={this.showPassword}
+							type={this.state.showPassword ? 'text' : 'password'}
+							name={'password'}
+							id={'password'}
+							autoComplete={'current-password'}
+							value={this.state.password}
+							onChange={this.passwordChanged}
+							onFocus={this.focusPassword}
+							onBlur={this.blurPassword}
 						/>
-					</CheckBoxWrapper>
+
+					<CheckBoxWrapper className="password-toggle">
+							<input
+								type={'checkbox'}
+								name={'showPassword'}
+								id={'showPassword'}
+								checked={this.state.showPassword}
+								onChange={this.showPassword}
+							/>
+						<span className="eye-toggle" />
+						</CheckBoxWrapper>
+					</div>
+
+					<div className={'step' + status.back} onClick={this.goBack}/>
+					<div className={'step' + status.next} onClick={this.showNext}/>
+					<button className={status.submit} type={'submit'}>Done</button>
 				</div>
 
-				<div className={'step' + status.back} onClick={this.goBack}>Back</div>
-				<div className={'step' + status.next} onClick={this.showNext}>Next</div>
-				<button className={status.submit} type={'submit'}>Submit</button>
+				<div className="page-numbers">
+					<span>{page.number}</span>
+					<span className="dash"/>
+					<span>3</span>
+				</div>
+
 
 				<CheckBoxWrapper style={{display: 'none'}}>
 					<input type={'checkbox'} name={'terms'} id={'terms'} />
