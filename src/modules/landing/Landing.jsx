b23fbe381a3eb2e784e3cacc369f9ee167d96763
Ryan Luu <ryan@vumble.com>
2018-09-04 16:43:26 -0400

    Landing page with Record and Input for Collection URL

--- src/modules/landing/Landing.jsx
+++ src/modules/landing/Landing.jsx
@@ -1,18 +1,27 @@
-import {Link} from 'react-router-dom';
 import React from 'react';
-
-import {ROUTES} from '../routes/routes';
+var rings = [];
+for (var i = 0; i < 80; i++) {
+	rings.push(<div className="ring" key={i} />);
+}
 
 export default function Landing() {
 	return (
-		<Link className="landing-inner" to={ROUTES.PLAYER}>
-			<div className="c1" />
-			<div className="c2" />
-			<div className="c3" />
-			<div className="c4" />
-			<div className="c5" />
-			<div className="c6" />
-			<h1>Listen</h1>
-		</Link>
+		<div className="landing-inner">
+			<div className="circle" id="center" />
+			{rings}
+			<div className="circle" id="outer" />
+			<div className="circle" id="background" />
+			<div className="circle" id="hole" />
+			<h1 className="hero-headline">Open music collections</h1>
+			<div className="hero-action">
+				<div className="link-holder">
+					<h3 className="link-base">openrecord.co/</h3>
+					<input type="text" placeholder="smoothvibes" />
+				</div>
+				<button className="hero-button" type="submit">
+					Open Collection
+				</button>
+			</div>
+		</div>
 	);
 }
