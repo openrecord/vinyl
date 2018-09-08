a958487f8f63bc578e2033cccd69264430da7a99
Ryan Luu <ryan@vumble.com>
2018-09-08 14:06:24 -0400

    Removed unneccesary 'Results' from UniplayerContainer

--- src/modules/uniplayer/UniplayerContainer.jsx
+++ src/modules/uniplayer/UniplayerContainer.jsx
@@ -2,10 +2,7 @@ import {connect} from 'react-redux';
 import React from 'react';
 
 import Uniplayer from './Uniplayer.jsx';
-import * as queueActions from '../queue/state';
 
 const mapStateToProps = ({player}) => player;
 
-export default connect(mapStateToProps)(({currentlyPlaying, results}) => (
-	<Uniplayer currentlyPlaying={currentlyPlaying} results={results} />
-));
+export default connect(mapStateToProps)(({currentlyPlaying}) => <Uniplayer currentlyPlaying={currentlyPlaying} />);
