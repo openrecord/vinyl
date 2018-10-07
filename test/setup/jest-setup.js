b9f4b0972b392f4f479521d43aabd64b5458b993
Chas DeVeas <chas@videoblocks.com>
2018-10-07 16:04:26 -0400

    Add jest and puppeteer setup code plus testUtils.

---
+++ test/setup/jest-setup.js
@@ -0,0 +1,39 @@
+const util = require('util');
+const config = require('config');
+const puppeteer = require('puppeteer');
+const fs = require('fs-extra');
+
+const tutil = require('./tools/testUtils');
+
+module.exports = async function() {
+	await tutil.ensureScreenshotsDir();
+	return config.puppeteer.useRemoteChrome ? discoverRunningChrome() : launchChromeWithPuppeteer();
+};
+
+async function discoverRunningChrome() {
+	console.log(
+		`Getting websocket information from running Chrome instance at ${
+			config.puppeteer.remoteChromeEndpoint
+		}`
+	);
+	const websocketEndpoint = await tutil.getWebsocketEndpointFromRemoteChrome();
+
+	console.log('Saving websocket endpoint to file: ' + websocketEndpoint);
+	tutil.writeWebsocketEndpointFile(websocketEndpoint);
+}
+
+async function launchChromeWithPuppeteer() {
+	console.log('Launching Chrome with puppeteer.');
+	const browser = await puppeteer.launch({
+		headless: false // tests seem to fail in headless mode if run in parallel
+	});
+
+	/**
+	 * @see https://jestjs.io/docs/en/puppeteer
+	 * store the browser instance so we can teardown it later
+	 * this global is only available in the teardown but not in TestEnvironments
+	 */
+	global.__BROWSER_GLOBAL__ = browser;
+
+	tutil.writeWebsocketEndpointFile(browser.wsEndpoint());
+}
---
+++ test/setup/puppeteer_environment.js
@@ -0,0 +1,24 @@
+const NodeEnvironment = require('jest-environment-node');
+const puppeteer = require('puppeteer');
+
+const tutil = require('./tools/testUtils');
+
+class PuppeteerEnvironment extends NodeEnvironment {
+	async setup() {
+		await super.setup();
+
+		const wsEndpoint = tutil.readWebsocketEndpointFile();
+
+		console.log(`Connecting to Chrome browser via websocket: ${wsEndpoint}`);
+		this.global.__BROWSER__ = await puppeteer.connect({
+			browserWSEndpoint: wsEndpoint
+		});
+	}
+
+	async teardown() {
+		super.teardown();
+		return this.global.__BROWSER__.disconnect();
+	}
+}
+
+module.exports = PuppeteerEnvironment;
