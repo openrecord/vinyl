4a23754b5c3d9cbada0d2bf4226409b2d7c9cd0e
jamesmcnamara <jamesscottmcnamara@gmail.com>
2018-10-18 13:37:32 -0700

    Move where onClickOutside exists

--- src/modules/search/components/Search.jsx
+++ src/modules/search/components/Search.jsx
@@ -2,45 +2,40 @@ import {VelocityTransitionGroup} from 'velocity-react';
 import React from 'react';
 import styled from 'styled-components';
 
-import onClickOutside from 'react-onclickoutside';
-
 import SearchBackground from './SearchBackground';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import * as animations from '../../common/animations';
 
-class Search extends React.Component {
-	handleClickOutside({target: {dataset: {id} = {id: null}}}) {
-		if (this.props.isSearchOpen) {
-			if (id !== 'show-hide-search') {
-				this.props.toggleSearch();
-			}
-			this.props.clearSearch();
-		}
-	}
-
-	render() {
-		const {query, setSearch, results, enqueue, isSearchOpen} = this.props;
-
-		return (
-			<SearchHolder>
-				<VelocityTransitionGroup
-					enter={{animation: animations.slideDownExpand.in, duration: 200}}
-					leave={animations.slideDownExpand.out}
-				>
-					{isSearchOpen && <SearchBar query={query} onChange={setSearch} />}
-				</VelocityTransitionGroup>
-				<SearchBackground isSearchOpen={isSearchOpen}>
-					{results.length > 0 && <SearchResults results={results} enqueue={enqueue} />}
-				</SearchBackground>
-			</SearchHolder>
-		);
-	}
+export default function Search({
+	query,
+	setSearch,
+	results,
+	enqueue,
+	isSearchOpen,
+	toggleSearch,
+	clearSearch
+}) {
+	return (
+		<SearchHolder>
+			<VelocityTransitionGroup
+				enter={{animation: animations.slideDownExpand.in, duration: 200}}
+				leave={animations.slideDownExpand.out}
+			>
+				{isSearchOpen && <SearchBar query={query} onChange={setSearch} />}
+			</VelocityTransitionGroup>
+			<SearchBackground
+				isSearchOpen={isSearchOpen}
+				toggleSearch={toggleSearch}
+				clearSearch={clearSearch}
+			>
+				{results.length > 0 && <SearchResults results={results} enqueue={enqueue} />}
+			</SearchBackground>
+		</SearchHolder>
+	);
 }
 
 const SearchHolder = styled.div`
 	background: rgb(36, 36, 36);
 	box-shadow: 0px 4px 6px 4px rgba(0, 0, 0, 0.1);
 `;
-
-export default onClickOutside(Search);
