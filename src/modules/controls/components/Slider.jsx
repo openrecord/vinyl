cf8a3f712eacb67e896316911cec0f1d8b428f44
jamesmcnamara <jamesscottmcnamara@gmail.com>
2018-10-25 01:46:53 -0700

    Switched to rc-slider to see if better performance

--- src/modules/controls/components/Slider.jsx
+++ src/modules/controls/components/Slider.jsx
@@ -1,5 +1,5 @@
+import ReactSlider from 'rc-slider';
 import * as React from 'react';
-import ReactSlider from 'react-slider';
 import styled from 'styled-components';
 
 import Duration from './Duration';
@@ -15,36 +15,41 @@ export default function Slider({played, duration, setPlayed}) {
 					value={played}
 					step={0.001}
 					onChange={setPlayed}
-					withBars
-				>
-					<Handle />
-				</StyledReactSlider>
+					handleStyle={handleStyle}
+				/>
 			</Flex1>
 			<Duration seconds={duration} />
 		</SliderFlex>
 	);
 }
 
-const Handle = styled.div`
-	background: white;
-	border-radius: 50%;
-	height: 0;
-	width: 0;
-	transition: all 0.3s linear;
-`;
+const handleStyle = {
+	background: 'white',
+	borderRadius: '50%',
+	height: '0.75rem',
+	width: '0.75rem',
+	position: 'relative',
+	transform: 'translate(-50%, -50%) scale(0)',
+	top: '-50%',
+	transition: 'transform 0.3s linear'
+};
 
 const SliderFlex = styled.div`
 	display: flex;
 	align-items: center;
+	cursor: pointer;
+
+	.rc-slider-handle:focus {
+		outline: 0;
+	}
 
 	:hover {
-		.bar-0 {
+		.rc-slider-track {
 			background: rgb(156, 77, 157);
 		}
 
-		${Handle} {
-			height: 0.75rem;
-			width: 0.75rem;
+		.rc-slider-handle {
+			transform: translate(-50%, -50%) scale(1) !important;
 		}
 	}
 `;
@@ -57,20 +62,14 @@ const StyledReactSlider = styled(ReactSlider)`
 	background: rgb(80, 80, 80);
 	margin: 0.75rem 0;
 
-	/* class that react-slider uses for already-elapsed protion */
-	.bar-0 {
+	.rc-slider-track {
 		background: rgba(255, 255, 255, 0.6);
-		transition: all 0.3s linear;
+		transition: background-color 0.3s linear;
 	}
 
 	&,
-	.bar-0 {
+	.rc-slider-track {
 		height: 0.25rem;
 		border-radius: 0.5rem;
-	}
-
-	.handle {
-		top: 50%;
-		transform: translate(-50%, -50%);
 	}
 `;
