050e2c99a8a1143cc8090de1ec970c95e72b963d
jamesmcnamara <jamesscottmcnamara@gmail.com>
2018-10-25 01:13:51 -0700

    JS Slider

--- src/modules/controls/components/Slider.jsx
+++ src/modules/controls/components/Slider.jsx
@@ -1,96 +1,76 @@
 import * as React from 'react';
+import ReactSlider from 'react-slider';
 import styled from 'styled-components';
+
 import Duration from './Duration';
 
 export default function Slider({played, duration, setPlayed}) {
 	return (
-		<SliderContainer>
+		<SliderFlex>
 			<Duration seconds={played * duration} />
-			<StyledSlider>
-				<ProgressBarBackground>
-					<Progress played={played} />
-				</ProgressBarBackground>
-				<Bar
-					type="range"
+			<Flex1>
+				<StyledReactSlider
 					min={0}
 					max={1}
-					step="any"
 					value={played}
-					onChange={({target: {value}}) => setPlayed(parseFloat(value))}
-				/>
-				;
-			</StyledSlider>
+					step={0.001}
+					onChange={setPlayed}
+					withBars
+				>
+					<Handle />
+				</StyledReactSlider>
+			</Flex1>
 			<Duration seconds={duration} />
-		</SliderContainer>
+		</SliderFlex>
 	);
 }
 
-const SliderContainer = styled.div`
+const Handle = styled.div`
+	background: white;
+	border-radius: 50%;
+	height: 0;
+	width: 0;
+	transition: all 0.3s linear;
+`;
+
+const SliderFlex = styled.div`
 	display: flex;
 	align-items: center;
-`;
-
-const ProgressBarBackground = styled.div`
-	background: rgb(80, 80, 80);
-	border-radius: 0.5rem;
-	height: 0.25rem;
-	overflow: hidden;
-	position: absolute;
-	top: 50%;
-	width: 100%;
-	transform: translateY(-50%);
-`;
-
-const Progress = styled.span.attrs({
-	style: props => ({right: `calc(100% - ${props.played * 100}%)`})
-})`
-	background: rgba(255, 255, 255, 0.6);
-	border-radius: 0 0.5rem 0.5rem 0;
-	height: 0.25rem;
-	position: absolute;
-	top: 50%;
-	width: 100%;
-	transform: translateY(-50%);
-`;
-
-const Bar = styled.input`
-	background: transparent;
-	appearance: none;
-	cursor: pointer;
-	height: 1.5rem;
-	margin: 0;
-	outline: none;
-	position: relative;
-	width: 100%;
-	transition: all 0.1s;
-
-	::-webkit-slider-thumb {
-		appearance: none;
-		background: white;
-		border-radius: 50%;
-		pointer-events: none;
-		transition: all 0.1s;
-		height: 0;
-		width: 0;
-	}
-`;
-
-const StyledSlider = styled.div`
-	height: 1.5rem;
-	position: relative;
-	transition: all 0.1s;
-	width: 100%;
 
 	:hover {
-		${Progress} {
+		.bar-0 {
 			background: rgb(156, 77, 157);
 		}
 
-		${Bar} {
-			::-webkit-slider-thumb {
-				height: 0.75rem;
-				width: 0.75rem;
-			}
+		${Handle} {
+			height: 0.75rem;
+			width: 0.75rem;
 		}
+	}
+`;
+
+const Flex1 = styled.div`
+	flex: 1;
+`;
+
+const StyledReactSlider = styled(ReactSlider)`
+	background: rgb(80, 80, 80);
+	margin: 0.75rem 0;
+
+	/* class that react-slider uses for already-elapsed protion */
+	.bar-0 {
+		background: rgba(255, 255, 255, 0.6);
+		transition: all 0.3s linear;
+	}
+
+	&,
+	.bar-0 {
+		height: 0.25rem;
+		border-radius: 0.5rem;
+	}
+
+	.handle {
+		top: 50%;
+		transform: translate(-50%, -50%);
 	}
 `;
