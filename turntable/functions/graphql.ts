71cec4b4be4b32f92450f21d573bad9e8ade51c2
jamesmcnamara <jamesscottmcnamara@gmail.com>
2018-12-04 15:40:20 -0800

    moved schemas around

--- turntable/functions/graphql.ts
+++ turntable/functions/graphql.ts
@@ -1,27 +1,24 @@
 import {ApolloServer, gql} from 'apollo-server-lambda';
 import {forwardTo} from 'graphql-binding';
 import {importSchema} from 'graphql-import';
-import fs from 'mz/fs';
 import {always, map, set} from 'shades';
 
 import {Prisma} from '../generated/prisma';
 import updateIndex from './mutations/updateIndex';
 
 declare const ENDPOINT: string;
+declare const IS_PRODUCTION: boolean;
 
 export const prisma = new Prisma({
 	endpoint: ENDPOINT
 });
 
-async function ls() {
-	console.log(await fs.readdir('.'));
-	console.log(await fs.readdir('..'));
-}
-ls();
 const forwardToPrisma = map(always(forwardTo('db')));
 
 const server = new ApolloServer({
-	typeDefs: gql(importSchema('server.graphql')),
+	typeDefs: gql(
+		importSchema(IS_PRODUCTION ? 'schemas/server.graphql' : 'turntable/server.graphql')
+	),
 	resolvers: {
 		Query: forwardToPrisma(prisma.query),
 		Mutation: {
--- turntable/webpack.config.js
+++ turntable/webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const OUTPUT_DIR = path.resolve(__dirname, '../dist');
+const FUNCTIONS_DIR = path.resolve(OUTPUT_DIR, 'functions');
+const SCHEMAS_DIR = path.resolve(FUNCTIONS_DIR, 'schemas');
 
 console.log('endpoint is', process.env.ENDPOINT);
 
@@ -8,7 +13,7 @@ module.exports = {
 	entry: ['babel-polyfill', './graphql.ts'],
 	output: {
 		filename: 'graphql.js',
-		path: path.resolve(__dirname, '../dist/functions')
+		path: FUNCTIONS_DIR
 	},
 	module: {
 		rules: [
@@ -38,8 +43,13 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.DefinePlugin({
-			ENDPOINT: JSON.stringify(process.env.ENDPOINT)
-		})
+			ENDPOINT: JSON.stringify(process.env.ENDPOINT),
+			IS_PRODUCTION: JSON.stringify(process.env.NODE_ENV === 'production')
+		}),
+		new CopyWebpackPlugin([
+			{from: '../schema.graphql', to: SCHEMAS_DIR},
+			{from: '../server.graphql', to: SCHEMAS_DIR}
+		])
 	],
 	optimization: {
 		minimize: false
