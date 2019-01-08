f624e42586715166e9bcfdc4e6c732bb1a7c7d5b
jamesmcnamara <jamesscottmcnamara@gmail.com>
2019-01-08 01:53:11 -0800

    Created a unified debouncing system

---
+++ src/modules/search/hooks/useDebouncedSearch.ts
@@ -0,0 +1,23 @@
+import * as _ from 'lodash';
+import * as React from 'react';
+
+export default function useDebouncedSearch<T>(
+  search: string,
+  searcher: (query: string) => Promise<T[]>
+): T[] {
+  const [results, setResults] = React.useState([] as T[]);
+  const debouncedSearch = React.useRef(_.debounce(search => searcher(search).then(setResults), 500))
+    .current;
+  React.useEffect(
+    () => {
+      if (search) {
+        debouncedSearch.cancel();
+        debouncedSearch(search);
+      } else {
+        setResults([]);
+      }
+    },
+    [search]
+  );
+  return results;
+}
